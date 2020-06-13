7ee08276305aeb2c12cb78487fd8745d8eaca6f6
scottsmithDS <shsmithsd@gmail.com>
2020-06-13 12:38:43 -0700

    commit

--- static/logicemily.js
+++ static/logicemily.js
@@ -1,45 +1,61 @@
 
-
-function selectData(data,year){
-    var results=[];
-    for (var i=0; i<data.length; i++){
-      if (data[i]['Indicator Name'] == 'CO2 emissions (metric tons per capita)'){
-        results.push([data[i]['Lat'], data[i]['Long'], data[i][year]]);
-      }
-    }
-    return results;
-  }
-
   
-  var jsonData = "static/Data/Untitled.json";
-  console.log(jsonData);
-  
-  
-      var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-          attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-          maxZoom: 10,
-          id: "mapbox/streets-v11",
-          accessToken: API_KEY
-      });
-  
-      // Create our map, giving it the streetmap and Coe levels layers to display on load
-      var myMap1 = L.map("map1", {
-          center: [
-            37.09, -95.71
-          ],
-          zoom: 3,
-        });
-  
-        streetmap.addTo(myMap1); 
-        var year='2014';
-      d3.json(jsonData, function(data) {
-          console.log(data);
-          console.log(selectData(data,year));
-  
-          var heat = L.heatLayer(selectData(data,year), {radius: 50, minOpacity:1, blur: 60 }).addTo(myMap1);
-  
-  
-  
-        });
-  
-  
+        var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+        // Define streetmap layers
+        var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            accessToken: API_KEY
+          });
+        // Create our map, giving it the streetmap and earthquakes layers to display on load
+        var myMap3 = L.map("map3", {
+            center: [
+              37.09, -95.71
+            ],
+            zoom: 5,
+          });
+          streetmap.addTo(myMap3);
+          d3.json(queryUrl, function(data) {
+            // Once we get a response, send the data.features object to the createFeatures function
+            console.log(data);
+            function style(feature){
+                return{
+                radius: getRadius(feature.properties.mag),
+                fillColor: getColor(feature.properties.mag),
+                color: 'black',
+                weight: 0.5,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+        }
+        function getColor(magnitude){
+            switch(true){
+                case magnitude > 5 :
+                    return 'red';
+                case magnitude > 4 :
+                    return 'orange';
+                case magnitude > 3 :
+                    return 'pink';
+                case magnitude > 2 :
+                    return 'yellow';
+                case magnitude >1 :
+                    return 'green';
+                default :
+                    return 'blue';
+            }
+        }
+        function getRadius(magnitude){
+            return magnitude*5;
+        }
+        /* reference https://leafletjs.com/examples/geojson/ */
+        L.geoJSON(data, {
+            pointToLayer: function (feature, latnlng) {
+                return L.circleMarker(latnlng);
+            },
+            style:style,
+            onEachFeature:function(feature,layer){
+                layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+            }
+        }).addTo(myMap3);
+      })
--- static/logicjessica.js
+++ static/logicjessica.js
@@ -1,45 +1,38 @@
-function selectData(data,year){
-  var results=[];
-    for (var i=0; i<data.length; i++){
-      if (data[i]['Indicator Name'] == 'CO2 emissions (metric tons per capita)'){
-        results.push([data[i]['Lat'], data[i]['Long'], data[i][year]]);
-      };
-    };
-  return results;
-};
-
-  
-var jsonData = "static/Data/Untitled.json";
-console.log(jsonData);
-  
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 10,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY
-});
-  
-// Create our map, giving it the streetmap and Coe levels layers to display on load
 var myMap2 = L.map("map2", {
-    center: [37.09, -95.71],
-    zoom: 3,
+  center: [34.0522, -118.2437],
+  zoom: 8
 });
-  
-streetmap.addTo(myMap2); 
 
-var year='2014';
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+  }).addTo(myMap2);
+// Adding tile layer
 
-d3.json(jsonData, function(data) {
-  console.log(data);
-  console.log(selectData(data,year));
-  
-  var choropleth = L.choropleth(selectData(data, year), {
+// Load in geojson data
+var geoData = "static/Data/class.geojson";
+
+var geojson;
+
+// Grab data with d3
+d3.json(geoData, function(data) {
+
+  // Create a new choropleth layer
+  geojson = L.choropleth(data, {
+
     // Define what  property in the features to use
-    valueProperty: year,
+    valueProperty: "MHI2016",
+
     // Set color scale
     scale: ["#ffffb2", "#b10026"],
+
     // Number of breaks in step range
     steps: 10,
+
     // q for quartile, e for equidistant, k for k-means
     mode: "q",
     style: {
@@ -48,14 +41,14 @@ d3.json(jsonData, function(data) {
       weight: 1,
       fillOpacity: 0.8
     },
+
     // Binding a pop-up to each layer
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("Country Name: " + feature.countryName + "<br>Forest area (% of land area):<br>" +
-        "$" + feature.properties["1993"]);
-      }
-    }).addTo(myMap2);
-  });
-  
+      layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
+        "$" + feature.properties.MHI2016);
+    }
+  }).addTo(myMap2);
+
   // Set up the legend
   var legend = L.control({ position: "bottomright" });
   legend.onAdd = function() {
@@ -65,7 +58,7 @@ d3.json(jsonData, function(data) {
     var labels = [];
 
     // Add min & max
-    var legendInfo = "<h1>Forest Area (% of Land Area)</h1>" +
+    var legendInfo = "<h1>Median Income</h1>" +
       "<div class=\"labels\">" +
         "<div class=\"min\">" + limits[0] + "</div>" +
         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
@@ -83,4 +76,5 @@ d3.json(jsonData, function(data) {
 
   // Adding legend to the map
   legend.addTo(myMap2);
+
 });
